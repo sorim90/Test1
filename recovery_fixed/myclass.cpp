750c05525482a7d2a35bd533b4f0beb76ec86a94
sorim90 <sorim90@yandex.com>
2017-02-18 05:36:20 -0800

    fixed recovery prj

---
+++ recovery_fixed/myclass.cpp
@@ -0,0 +1,36 @@
+#include "myclass.h"
+#include "qjoystick.h"
+#include <QDebug>
+
+Myclass *p_instance = 0;
+
+Myclass::Myclass(QObject *parent) : QObject(parent)
+{
+    QJoystick *joystick = QJoystick::openJoystick("/dev/input/js0", 1);
+    if (joystick) {
+        qDebug() << "ok";
+    } else {
+        qDebug() << "bad";
+    }
+    connect(joystick, SIGNAL(axisChanged(int,short)), this, SLOT(eventAxisFunc()));
+    connect(joystick, SIGNAL(buttonChanged(int,bool)), this, SLOT(eventButtonFunc()));
+}
+
+Myclass* Myclass::instance()
+{
+    if (!p_instance)
+    {
+        p_instance = new Myclass();
+        return p_instance;
+    }
+}
+
+void Myclass::eventAxisFunc()
+{
+
+}
+
+void Myclass::eventButtonFunc()
+{
+    f_flag = true;
+}
